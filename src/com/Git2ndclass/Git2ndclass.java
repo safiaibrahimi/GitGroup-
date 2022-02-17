c440c22787f0af1a8630948451a9c59c5a0b3e3e
Safia Ibrahimi <inrahimi.safia786@gmail.com>
2022-02-17 13:27:32 -0500

    1st commit

---
+++ src/com/Git2ndclass/Git2ndclass.java
@@ -0,0 +1,13 @@
+package com.Git2ndclass;
+
+public class Git2ndclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello second class");
+		System.out.println("The weather is sunny today");
+		System.out.println("The weather is cold today");
+		System.out.println(" Hello");
+	}
+
+}
